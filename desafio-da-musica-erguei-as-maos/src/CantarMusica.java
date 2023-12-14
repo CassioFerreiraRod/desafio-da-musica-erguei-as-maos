af3d2660d036521486e1e4356e5188849f17e8d3
Cassio Ferreira <cassio1215@hotmail.com>
2023-12-14 17:58:14 -0300

    Refatorando métodos e adicinonando comentários

--- desafio-da-musica-erguei-as-maos/src/CantarMusica.java
+++ desafio-da-musica-erguei-as-maos/src/CantarMusica.java
@@ -1,155 +1,188 @@
 public class CantarMusica {
-
-    // Método que imprime o primeiro trecho da múscia
-    public void primeiroTrecho(boolean ehPorIsso, int repetir) {
-        // Condicional que imprime "Por isso...!" caso a varivel ehPorIsso seja verdadeira, senão, imprime nada
-        System.out.println(ehPorIsso ? "Por isso...!" : "");
-
-        // Loop que que imprime a quantidade de vezes de acordo com o agumento repetir do método
+    // Método que imprime o primeiro trecho da música de acordo com os argumentos que serão usados
+    public void imprimirPrimeiroTrecho(boolean imprimirPorIsso, int repetir) {
+        // Condicional que imprime "Por isso...!" caso a variável ehPorIsso seja verdadeira, senão, imprime nada
+        System.out.println(imprimirPorIsso ? "Por isso...!" : "");
+        // Loop que imprime essa estrofe repetidas vezes, com base no valor fornecido pelo parâmetro repetir
         for (int i = 0; i < repetir; i++) {
-            // Looop que imprime duas vezes a cada iteração do loop principal
+          // Loop que imprime o verso 'Erguei as mãos e dai glória a Deus' repetidas vezes, com base no valor fornecido
             for (int j = 0; j < 2; j++) {
                 System.out.println("Erguei as mãos e dai glória a Deus");
             }
             System.out.println("Erguei as mãos");
             System.out.println("E cantai como os filhos do Senhor\n");
         }
-
     }
 
-    // Método que imprime o segundo trecho da múscia
-    public void segundoTrecho(int posicao) {
-        // Criando array para salvar o nome do animanis
+    // Método que imprime o segundo trecho da música
+    public void imprimirSegundoTrecho() {
+        // Criando array para salvar o nome do animais
         String[] primeirosAnimais = {"elefante", "minhoquinha", "canguru"};
         String[] segundosAnimais = {"passarinhos", "pinguins", "sapinho"};
-
-        // Loop que verifica se o argumento posição é menor ou igual a 2
-        while (posicao <= 2) {
-            // Loop que que imprime 2 vezes a linha3
-            for (int i = 0; i < 2; i++) {
+        // Loop que verifica cada posição até o tamanho dos arrays e imprime a estrofe
+        for (int i = 0; i < primeirosAnimais.length; i++) {
+            // Loop que imprime 2 vezes o verso
+            for (int j = 0; j < 2; j++) {
                 System.out.println("Os animaizinhos subiram de dois em dois");
             }
-            // Imprime o verso e substitui de acordo com a posição das array
-            System.out.printf("O %s\n", primeirosAnimais[posicao]);
-            System.out.printf("E os %s, como os filhos do Senhor\n\n", segundosAnimais[posicao]);
-            posicao++; // icrementa o argumento para continuar o laço
+            // Imprime o verso de acordo com a posição das array
+            System.out.printf("O %s\n", primeirosAnimais[i]);
+            System.out.printf("E os %s, como os filhos do Senhor\n\n", segundosAnimais[i]);
         }
     }
 
-    // Método que imprime o terceiro trecho da múscia
-    public void terceiroTrecho() {
-
-        // Looop que imprime duas vezes a cada vez que o método é invocado
+    // Método que imprime o terceiro trecho da música
+    public void imprimirTerceiroTrecho() {
+        // Loop que imprime o verso duas vezes a cada vez que o método é invocado
         for (int i = 0; i < 2; i++) {
             System.out.println("Deus disse a Noé: Constrói uma arca");
         }
-        System.out.println("Que seja feita");
-        System.out.println("De madeira para os filhos do Senhor\n");
+        // Imprime o restante da estrofe
+        System.out.println("""
+                Que seja feita
+                De madeira para os filhos do Senhor
+                """);
     }
 
-    // Método que imprime o quarto trecho da múscia
-    public void quartoTrecho() {
-        System.out.println("O senhor tem muitos filhos");
-        System.out.println("Todos filhos ele tem");
-        System.out.println("Eu sou um deles, você também");
-        System.out.println("Louvemos ao senhor\n");
+    // Método que imprime o quarto trecho da música de acordo com os parâmetros
+    public void imprimirQuartoTrecho(boolean imprimirBracos, int repetir) {
 
-    }
-
-    // Método que imprime o quinto trecho da múscia
-    public void quintoTrecho(boolean ehBraco, int repetir) {
-
-        // Looop que imprime duas vezes a cada chamada do método
+        // Loop que imprime essa estrofe repetidas vezes, com base no valor fornecido pelo parâmetro repetir
         for (int i = 0; i < repetir; i++) {
             System.out.println("O senhor tem muitos filhos");
             System.out.println("Muitos filhos ele tem");
             System.out.println("Eu sou um deles, você também");
             System.out.println("Louvemos ao senhor\n");
         }
-        // Condicional que imprime "Braço direito, braço esquerdo" caso a varivel ehBraco seja verdadeira,
+
+        // Condicional que imprime "Braço direito, braço esquerdo" caso a variável imprimirBracos seja verdadeira,
         // senão, imprime nada
-        System.out.println(ehBraco ? "Braço direito, braço esquerdo\n" : "");
+        System.out.print(imprimirBracos ? "Braço direito, braço esquerdo\n\n" : "");
     }
 
-    // Método que imprime o sexto trecho da múscia
-    public void sextoTrecho(boolean ehBraco) {
-
-        // Condicional que imprime "Braço direito, braço esquerdo Perna direita" caso a varivel ehBraco seja verdadeira,
-        // senão, imprime nada
-        System.out.println(ehBraco ? """
+    // Método que imprime o quinto trecho da música
+    public void imprimirQuintoTrecho() {
+        // Imprime o verso
+        System.out.println("""
                 Braço direito, braço esquerdo
                 Perna direita
-                """ : "");
-
-        // Invocando o método quinto trecho
-        quintoTrecho(false, 1);
-
-    }
-
-    // Método que imprime o sétimo trecho da múscia
-    public void setimoTrecho(boolean ehTrechoFinal) {
-        // Condicional que imprime o verso sem quebra de linha caso a varivel ehTrechoFinal seja verdadeira
-        // senão, imprime com quebra de linha
-        System.out.println(ehTrechoFinal ? """
-                Braço direito, braço esquerdo
-                Perna direita, perna esquerda""" : """
-                Braço direito, braço esquerdo
-                Perna direita, perna esquerda
                 """);
     }
 
-    // Método que imprime o oitavo trecho da múscia
-    public void oitavoTrecho(boolean ehBalanca) {
-        // Condicional que imprime "Balança a cabeça" caso a varivel ehBalanca seja verdadeira,
-        // senão, imprime nada
-        System.out.println(ehBalanca ? "Balança a cabeça" : " ");
-
-        // Invocando o método quintoTrecho()
-        quintoTrecho(false, 1);
-        // Invocando o método sétimoTrecho()
-        setimoTrecho(false);
+    // Método que imprime o sexto trecho da música
+    public void imprimirSextoTrecho(boolean ehQuebraDeLinha) {
+        // Criando uma variável para salvar o verso
+        String verso = """
+                Braço direito, braço esquerdo
+                Perna direita, perna esquerda""";
+        // Condicional que imprime o verso com quebra de linha caso o parâmetro ehQuebraDeLinha seja verdadeira
+        // senão, imprime sem quebra de linha
+        System.out.println(ehQuebraDeLinha ? verso +"\n": verso);
     }
 
-    // Método que imprime o nono trecho da múscia
-    public void nonoTrecho() {
+    // Método que imprime o sétimo trecho da música
+    public void imprimirSetimoTrecho() {
+        // Imprime o verso da música
+        System.out.println("Balança a cabeça");
+    }
+
+    // Método que imprime o oitavo trecho da música
+    public void imprimirOitavoTrecho() {
+        // Imprime o verso da música
         System.out.println("Balança a cabeça, dá uma voltinha");
     }
 
-    // Método que imprime o decimo trecho da múscia
-    public void decimoTrecho(boolean ehAbraco) {
-        // Condicional que imprime "Da um pulinho e abraça o irmão" caso a varivel ehAbraco seja verdadeira,
-        // senão, imprime "Da um pulinho"
-        System.out.println(ehAbraco ? "Da um pulinho e abraça o irmão" : "Da um pulinho\n");
+    // Método que imprime o nono trecho da música
+    public void imprimirNonoTrecho(boolean ehAbraco) {
+        // Condicional que imprime "Dá um pulinho e abraça o irmão" caso o parâmetro ehAbraco seja verdadeira,
+        // senão, imprime "Dá um pulinho"
+        System.out.println(ehAbraco ? "Dá um pulinho e abraça o irmão\n" : "Dá um pulinho\n");
     }
 
-    // Método que imprime a primeira parte da múscia
+    // Método que imprime a primeira parte da música
     public void reproduzirPrimeiraParte() {
-        primeiroTrecho(false, 1);
-        segundoTrecho(0);
-        terceiroTrecho();
-        primeiroTrecho(true, 3);
+        // Invocando imprimirPrimeiroTrecho() com argumento 'false' para não imprimir a parte 'Por isso...!'
+        // e com o argumento '1' para quantidade de vezes
+        imprimirPrimeiroTrecho(false, 1);
+
+        // Invocando imprimirSegundoTrecho() para imprimir o trecho da música
+        imprimirSegundoTrecho();
+
+        // Invocando imprimirTerceiroTrecho() para imprimir o trecho da música
+        imprimirTerceiroTrecho();
+
+        // Invocando imprimirPrimeiroTrecho() com argumento 'true' para não imprimir a parte 'Por isso...!'
+        // e com o argumento '3' para quantidade de vezes
+        imprimirPrimeiroTrecho(true, 3);
     }
 
-    // Método que imprime a segunda parte da múscia
+    // Método que imprime a segunda parte da música
     public void reproduzirSegundaParte() {
-        quartoTrecho();
-        quintoTrecho(true, 1);
-        quintoTrecho(false, 1);
-        sextoTrecho(true);
-        setimoTrecho(false);
-        sextoTrecho(false);
-        setimoTrecho(false);
-        oitavoTrecho(true);
-        nonoTrecho();
-        quintoTrecho(false, 1);
-        setimoTrecho(true);
-        nonoTrecho();
-        decimoTrecho(false);
-        quintoTrecho(false, 1);
-        setimoTrecho(true);
-        nonoTrecho();
-        decimoTrecho(true);
+        // Invocando imprimirQuartoTrecho() com argumento 'true' para imprimir a parte 'Braço direito, braço esquerdo'
+        // e com o argumento '2' para quantidade de vezes
+        imprimirQuartoTrecho(true, 2);
 
+        // Invocando o imprimirQuartoTrecho() com argumento 'false' para não imprimir a parte 'Braço direito, braço esquerdo'
+        // e com o argumento '1' para quantidade de vezes
+        imprimirQuartoTrecho(false, 1);
+
+        // Invocando o imprimirQuintoTrecho() para imprimir o trecho da música
+        imprimirQuintoTrecho();
+
+        // Invocando o imprimirQuartoTrecho() com argumento 'false' para não imprimir a parte 'Braço direito, braço esquerdo'
+        // e com o argumento '1' para quantidade de vezes
+        imprimirQuartoTrecho(false, 1);
+
+        // Invocando o imprimirSextoTrecho() com argumento 'true' para imprimir o verso com quebra de linha
+        imprimirSextoTrecho(true);
+
+        // Invocando o imprimirQuartoTrecho() com argumento 'false' para não imprimir a parte 'Braço direito, braço esquerdo'
+        // e com o argumento '1' para quantidade de vezes
+        imprimirQuartoTrecho(false, 1);
+
+        // Invocando o imprimirSextoTrecho() com argumento 'true' para imprimir o verso com quebra de linha
+        imprimirSextoTrecho(true);
+
+        // Invocando o imprimirSetimoTrecho() para imprimir o trecho da música
+        imprimirSetimoTrecho();
+
+        // Invocando o imprimirQuartoTrecho() com argumento 'false' para não imprimir a parte 'Braço direito, braço esquerdo'
+        // e com o argumento '1' para quantidade de vezes
+        imprimirQuartoTrecho(false, 1);
+
+        // Invocando o imprimirSextoTrecho() com o argumento 'true' para imprimir o verso com quebra de linha
+        imprimirSextoTrecho(true);
+
+        // Invocando o imprimirOitavoTrecho() para imprimir o trecho da música
+        imprimirOitavoTrecho();
+
+        // Invocando o imprimirQuartoTrecho() com argumento 'false' para não imprimir a parte 'Braço direito, braço esquerdo'
+        // e com o argumento '1' para quantidade de vezes
+        imprimirQuartoTrecho(false, 1);
+
+        // Invocando o imprimirSextoTrecho() com argumento 'false' para imprimir o verso sem quebra de linha
+        imprimirSextoTrecho(false);
+
+        // Invocando o imprimirOitavoTrecho() para imprimir o trecho da música
+        imprimirOitavoTrecho();
+
+        // Invocando o nonoTrecho() com argumento 'false' para imprimir o verso 'Dá um pulinho'
+        // e não 'Dá um pulinho e abraça o irmão'
+        imprimirNonoTrecho(false);
+
+        // Invocando o imprimirQuartoTrecho() com argumento 'false' para não imprimir a parte 'Braço direito, braço esquerdo'
+        // e com o argumento '1' para quantidade de vezes
+        imprimirQuartoTrecho(false, 1);
+
+        // Invocando o imprimirSextoTrecho() com argumento 'false' para imprimir o verso sem quebra de linha
+        imprimirSextoTrecho(false);
+
+        // Invocando o imprimirOitavoTrecho() para imprimir o trecho da música
+        imprimirOitavoTrecho();
+
+        // Invocando o imprimirNonoTrecho() com argumento 'true' para imprimir o verso 'Dá um pulinho e abraça o irmão'
+        // e não 'Dá um pulinho'
+        imprimirNonoTrecho(true);
     }
 
 }
--- desafio-da-musica-erguei-as-maos/src/Menu.java
+++ desafio-da-musica-erguei-as-maos/src/Menu.java
@@ -2,40 +2,77 @@
 import java.util.Scanner;
 
 public class Menu {
-
-
-    public void iniciarMenu() {
-        CantarMusica musica = new CantarMusica();
-        System.out.println("""
-                Desafio da musica erguei as mãos
-                Como deseja reproduzir a música ?
-                1 - para a musica toda
-                2 - para somente a primeira parte
-                3 - para somente a segunda parte
-                """);
-        Scanner scanner = new Scanner(System.in);
-        String opcao = scanner.next();
-        switch (opcao) {
-            case "1":
-                musica.reproduzirPrimeiraParte();
-                System.out.println("E atenção agora, porque\n");
-                musica.reproduzirSegundaParte();
-                break;
-            case "2":
-                musica.reproduzirPrimeiraParte();
-                System.out.println("E atenção agora, porque");
-                break;
-            case "3":
-                System.out.println("E atenção agora, porque\n");
-                musica.reproduzirSegundaParte();
-                break;
-            default:
-                System.out.println("Opção Inválida. Digite uma opção válida\n");
-                iniciarMenu();
-                break;
-        }
+    // Criando um atributo do tipo CantarMusica
+    private CantarMusica musica;
+    // Construtor que irá instanciar CantarMusica() que irá atribuir ao atributo musica sempre que for invocado
+    public Menu() {
+        this.musica = new CantarMusica();
     }
+    // Método que irá iniciar e gerenciar o menu de interação
+    public void iniciarMenu() {
+        // Instanciando a classe Scanner para a entrada de dados
+        Scanner scanner = new Scanner(System.in);
+        // Variável que controla a continuidade do menu
+        boolean continuar = true;
 
+        // Bloco de tratamento de exceção para lidar com possíveis erros
+        try {
+            // Loop que mantém a execução do menu enquanto a variável 'continuar' permanecer verdadeira
+            while (continuar) {
+                // Imprime as opções para reprodução da música
+                System.out.println("""
+                        Desafio da música erguei as mãos
+                        Como deseja reproduzir a música ?
+                        1 - para a musica toda
+                        2 - para somente a primeira parte
+                        3 - para somente a segunda parte
+                        4 - para sair
+                        """);
+                // Variável que recebe a entrada do Scanner
+                String opcao = scanner.next();
+                // Bloco que verifica as entradas recebidas na variável 'opção'
+                switch (opcao) {
+                    case "1":
+                        // Reproduz a primeira parte da música
+                        musica.reproduzirPrimeiraParte();
+                        // Imprime o verso que divide a primeira parte da segunda
+                        System.out.println("E atenção agora, porque\n");
+                        // Reproduz a segunda parte da música
+                        musica.reproduzirSegundaParte();
+                        break;
+                    case "2":
+                        // Reproduz a primeira parte da música
+                        musica.reproduzirPrimeiraParte();
+                        // Imprime o verso que divide a primeira parte da segunda
+                        System.out.println("E atenção agora, porque\n");
+                        break;
+                    case "3":
+                        // Imprime o verso que divide a primeira parte da segunda
+                        System.out.println("E atenção agora, porque\n");
+                        // Reproduz a segunda parte da música
+                        musica.reproduzirSegundaParte();
+                        break;
+                    case "4":
+                        // Recebe o valor false caso a entrada seja '4' e encerra o bloco
+                        continuar = false;
+                        // Saindo do Menu
+                        break;
+                    default:
+                        // Imprime caso não seja uma opção esperada
+                        System.out.println("Opção Inválida. Digite uma opção válida\n");
+                        break;
+                }
+            }
+
+        } // Captura uma exceção do tipo IllegalStateException,
+          // geralmente lançada quando há operações inválidas no scanner
+        catch (IllegalStateException e) {
+            // Imprime essa mensagem caso aconteça algum erro
+            System.out.println("Erro, encerrando o sistema.");
+        }
+        // Finaliza o scanner
+        scanner.close();
+    }
 }
 
 
